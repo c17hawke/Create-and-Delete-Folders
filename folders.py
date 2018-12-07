4819124b6eb99222ece7c975d2e96f593ba0b00b
SUNNY BHAVEEN CHANDRA <34603575+c17hawke@users.noreply.github.com>
2018-12-07 17:39:24 +0530

    Add files via upload

---
+++ folders.py
@@ -0,0 +1,14 @@
+import sys
+def main():
+	yourChoice = input("enter\n 'm' to make folders\n 'r' to remove folders\n")
+	if yourChoice == 'm':
+		import makeFolders
+		makeFolders.make()
+	elif yourChoice == 'r':
+		import removeFolders
+		removeFolders.delete()
+	else:
+		print('exiting !!')
+		sys.exit()
+if __name__ == '__main__':
+	main()
