8a30c85fcd556c3dafa7de35af380e058d0a33f9
SUNNY BHAVEEN CHANDRA <34603575+c17hawke@users.noreply.github.com>
2018-12-07 17:08:10 +0530

    Add files via upload

---
+++ makeFolders.py
@@ -0,0 +1,53 @@
+import os 
+import sys
+
+def start():
+	'''start sequence'''
+	start_folder = input('starting folder seq \n')
+	if len(start_folder) > 0:
+		start_folder = int(start_folder)
+	else:
+		start_folder = 1
+	return start_folder
+
+def creating(start_folder, end_folder, common_name):
+	'''creates new folders'''
+	for i in range(start_folder ,end_folder + 1):
+		try:
+			if i < 10:
+				os.system('mkdir 0'+ str(i) + common_name)
+			else:
+				os.system('mkdir '+ str(i) + common_name)
+		except Exception as e:
+			pass
+
+def validate(start_folder,end_folder):
+	'''validate the sequence'''
+	if start_folder>end_folder:
+		print("relaunch again!!")
+		sys.exit()
+
+def main():
+	start_folder = start()
+
+	end_folder = int(input('how many folders you need \n'))
+	try:
+		validate(start_folder,end_folder)
+
+		common_name = input('suggest a common name for the folders \n')
+
+		if len(common_name) > 0:
+			pass
+		else:
+			# default case
+			print("No input given.\n Initiating default case!!\n")
+			common_name = 'folder'
+
+
+			creating(start_folder, end_folder, common_name)
+	finally:
+		print("DONE !!")
+
+
+if __name__ == '__main__':
+	main()
---
+++ removeFolders.py
@@ -0,0 +1,59 @@
+import os 
+import sys
+
+def end():
+	'''this selects and end folder seq'''
+	for i in range(2):
+		num_of_folder = input('how many folder you want to remove \n')
+		if len(num_of_folder) > 0:
+			num_of_folder = int(num_of_folder)
+			break
+		elif i == 1:
+			print("Nothing will be deleted ! Hence exiting the program")
+			sys.exit()
+		else:
+			print("please give an input, you have one more chance left\n")
+	return num_of_folder
+
+def removing(start_folder,num_of_folder,common_name):
+	'''this function deletes the folders'''
+	for i in range(start_folder,num_of_folder+1):
+		try:
+			if i < 10:
+				os.system('rm -r 0'+ str(i) + common_name)
+			else:
+				os.system('rm -r '+ str(i) + common_name)
+		except:
+			print("folder doesn't exists")
+
+def validate(start_folder,num_of_folder):
+	if start_folder>num_of_folder:
+		print("relaunch again")
+		sys.exit()
+
+def main():
+	start_folder = input('starting folder seq \n')
+
+	if len(start_folder) > 0:
+		start_folder = int(start_folder)
+	else:
+		print("No input given.\n Initiating default case!!\n")
+		start_folder = 1
+
+	try:
+		num_of_folder = end()
+		validate(start_folder,num_of_folder)
+		common_name = input('suggest a common name for the folders to remove \n')
+
+		if len(common_name) > 0:
+			pass
+		else:
+			print("No input given.\n Initiating default case!!\n")
+			common_name = 'folder'
+			removing(start_folder,num_of_folder,common_name)
+
+	finally:
+		print("DONE !!")	
+
+if __name__ == '__main__':
+	main()
